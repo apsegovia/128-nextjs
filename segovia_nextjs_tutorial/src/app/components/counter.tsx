4a3ac8b91dc45a46ef7ca542e7ada704592060a1
apsegovia <apsegovia@up.edu.ph>
2025-03-05 22:56:01 +0800

    added: client side authentication

--- segovia_nextjs_tutorial/src/app/components/counter.tsx
+++ segovia_nextjs_tutorial/src/app/components/counter.tsx
@@ -1,11 +1,18 @@
-"use client"
+"use client";
 import { useState } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 export const Counter = () => {
-   console.log("Counter component");
-   const [count, setCount] = useState(0);
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
 
-   return (
-      <button onClick={() => setCount(count + 1)}> Clicked {count} times</button>
-   );
+  console.log("Counter component");
+  const [count, setCount] = useState(0);
+
+  if (!isLoaded || !userId) {
+    return null;
+  }
+
+  return (
+    <button onClick={() => setCount(count + 1)}> Clicked {count} times</button>
+  );
 };
